3e3336a2d4ad8d0d33c86ad101b3f7eb3ffb2723
Roman Vozniuk <muzzlestore@gmail.com>
2021-06-13 23:20:46 +0300

    add form style

--- js/modal.js
+++ js/modal.js
@@ -2,20 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    explosionModalBtn: document.querySelector('[data-modal-explosion]')
+    modal: document.querySelector('[data-modal]')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.explosionModalBtn.addEventListener('click', explosionModal);
-  function toggleModal() {
-    refs.modal.classList.remove('is-explosion');     
-    refs.modal.classList.toggle('is-hidden');  
 
-  }
-  function explosionModal() {
-    refs.modal.classList.toggle('is-hidden'); 
-    refs.modal.classList.toggle('is-explosion');
+  function toggleModal() { 
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
